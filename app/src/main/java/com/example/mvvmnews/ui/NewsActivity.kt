7d86529397f31f615fedd56154de8e78b13f28cc
unknown <vedstandard@runbox.com>
2022-05-23 21:28:21 +0200

    layouts

--- app/src/main/java/com/example/mvvmnews/ui/NewsActivity.kt
+++ app/src/main/java/com/example/mvvmnews/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.mvvmnews
+package com.example.mvvmnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmnews.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
